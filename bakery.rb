8ea7f00afdddd350fea4514d5fe091c4c506178f
dennys <dantunish@nyc.yearup.com>
2018-12-13 14:42:52 -0500

    final draft of ruby

--- bakery.rb
+++ bakery.rb
@@ -10,7 +10,6 @@ class Cake
     attr_accessor :image
     attr_accessor :description
     attr_accessor :price
-
     def initialize(name, image, description, price)
         @name = name
         @image = image
@@ -28,6 +27,7 @@ get '/cake' do
 ]
     erb :cake
 end
+
 #  Muffin erb stuff
 class Muffin 
     attr_accessor :nam
@@ -42,32 +42,23 @@ class Muffin
     end
 end
 
+
+
 get '/muffin' do
     @river = [
-        Muffin.new('Greek YOugurt Chocolate Muffin', 'https://thebusybaker.ca/wp-content/uploads/2018/08/best-ever-greek-yogurt-chocolate-chip-muffins-fbig3.jpg', 'Greak Muffin Outsourced from the best', '20.00'),
-        Muffin.new('Oatmeal Healthy Muffin', 'https://dailyburn.com/life/wp-content/uploads/2016/02/Pumpkin-Spice-Oatmeal-Muffins-Recipe.jpg', 'Exoitc Muffin from Indonesia', '15.00'),
-        Muffin.new('Crazy Muffins!!', 'https://3.bp.blogspot.com/-3wgtFuSDyPk/WC4ye0a0g7I/AAAAAAAAek0/WGUPHR35OPU-NHv1GZcfvKi_i3i9kI00QCLcB/s1600/Crazy%2BCake%2BCupcakes%2B3.jpg', 'Even we Don\'t Know What\'s inside!?!?', '25.00')
-    ]
-
-    for a in @river
-        puts a
-        puts a.nam
-        puts  a.imag
-        puts  a.descriptio
-        puts  a.pric
-    end
-
+    Muffin.new('Greek YOugurt Chocolate Muffin', 'https://thebusybaker.ca/wp-content/uploads/2018/08/best-ever-greek-yogurt-chocolate-chip-muffins-fbig3.jpg', 'Greak Muffin Outsourced from the best', '20.00'),
+    Muffin.new('Oatmeal Healthy Muffin', 'https://dailyburn.com/life/wp-content/uploads/2016/02/Pumpkin-Spice-Oatmeal-Muffins-Recipe.jpg', 'Exoitc Muffin from Indonesia', '15.00'),
+    Muffin.new('Crazy Muffins!!', 'https://3.bp.blogspot.com/-3wgtFuSDyPk/WC4ye0a0g7I/AAAAAAAAek0/WGUPHR35OPU-NHv1GZcfvKi_i3i9kI00QCLcB/s1600/Crazy%2BCake%2BCupcakes%2B3.jpg', 'Even we Don\'t Know What\'s inside!?!?', '25.00')
+]
     erb :muffin
-
 end
+
 # Cookies erb stuff
 class Cookies
-
     attr_accessor :name
     attr_accessor :image
     attr_accessor :description
     attr_accessor :price
-
     def initialize(name, image, description, price)
         @name = name
         @image = image
@@ -80,16 +71,16 @@ get '/cookie' do
     @rainbow = [
         Cookies.new('Macedonian Cookie', 'https://i0.wp.com/www.livewellbakeoften.com/wp-content/uploads/2018/02/White-Chocolate-Macadamia-Nut-Cookies-3.jpg?resize=1360%2C2040', 'Taken straight from the source', '2.00'),
         Cookies.new('Hamburger Cookies', 'https://tryityoumightlikeit.files.wordpress.com/2011/08/hamburger-cookies-final.jpg', 'Cooked from the oven and came out medium rare', '5.00'),
-        Cookies.new('dOUble CHOCOLATE CHIP COOKIES', 'https://www.thereciperebel.com/wp-content/uploads/2016/01/Moms-Double-Chocolate-Cookies-www.thereciperebel.com-1-of-2.jpg', 'DOUBLE THE CHOCOLATE!! DOUBLE THE MADNESS!!', '4.00')
+        Cookies.new('DOUble CHOCOLATE CHIP COOKIES', 'https://www.thereciperebel.com/wp-content/uploads/2016/01/Moms-Double-Chocolate-Cookies-www.thereciperebel.com-1-of-2.jpg', 'DOUBLE THE CHOCOLATE!! DOUBLE THE MADNESS!!', '4.00')
     ]
     erb :cookie
 end
 
 
 
-CSV.open("bakery.csv", "wb") do |csv|
-    csv << ['People Cake', 'https://i.imgur.com/s3mg809.jpg', 'Grown Organically', '50.00']
+# CSV.open("bakery.csv", "wb") do |csv|
+#     csv <<  Cookies.new('Macedonian Cookie', 'https://i0.wp.com/www.livewellbakeoften.com/wp-content/uploads/2018/02/White-Chocolate-Macadamia-Nut-Cookies-3.jpg?resize=1360%2C2040', 'Taken straight from the source', '2.00')
 
-end
+# end
 
 
